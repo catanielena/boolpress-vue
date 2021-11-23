994bbdb747f10fed514453aead04faee22a682ea
catanielena <catani.elena1@gmail.com>
2021-11-23 18:25:01 +0100

    Add: Posts_list

---
+++ resources/js/guest/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <Header/>
+      <main>
+          <Posts/>
+      </main>
+      <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Posts from './components/Posts.vue';
+
+
+export default {
+    name: 'App',
+    components: {
+        Header, Footer, Posts
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/guest/components/Posts.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="post in posts" :key="post.id">{{post.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Posts',
+    data() {
+        return {
+            posts: []
+        }
+    },
+    mounted() {
+        axios.get('/api/posts')
+        .then((response) => {
+            this.posts = response.data.data;
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+}
+</script>
